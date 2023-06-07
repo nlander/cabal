39bd16a6d921aa529ab51cb735ec2fa23ea2dd15
Elodie Lander <lander89@gmail.com>
2023-06-07 10:55:46 -0500

    Add check of local library hie installation

--- cabal-testsuite/HieTests/install.test.hs
+++ cabal-testsuite/HieTests/install.test.hs
@@ -1,4 +1,9 @@
+import Distribution.Simple.LocalBuildInfo (absoluteInstallDirs, libdir, localPkgDescr, CopyDest(NoCopyDest))
 import Test.Cabal.Prelude
+
 main = setupAndCabalTest $ do
     withPackageDb $ do
         setup_install []
+        lbi <- getLocalBuildInfoM
+        let installedLibPath = libdir $ absoluteInstallDirs (localPkgDescr lbi) lbi NoCopyDest
+	shouldExist $ installedLibPath </> "Hie.hie"
